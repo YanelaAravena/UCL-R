8a05c74e78f3b71bf3ec7ff8d3b68410d58bfe3c
YanelaAravena <yanela.aravena@gmail.com>
2023-06-13 17:26:31 +0100

    Test2

---
+++ Rtest.R
@@ -0,0 +1,22 @@
+## R Club - Getting Started in R for Epidemiology ##
+
+# Chapter 1: Getting started, bringing in data & exploring your data
+# Chapter 2: Visualising further: Summarising your data before or after cleaning
+# Chapter 3: Data Cleaning
+# Chapter 4: Basic Visualisation / Normality checks 
+# Chapter 5: Modularizing' a repetitive cleaning task 
+# Chapter 6: Quickstart Table 1 & Extracting values for table-building 
+#
+#
+
+
+
+##----- [Chapter 1]: Getting started, bringing in Data & exploring your data -----##
+
+# Install & Loading packages
+library(haven)
+install.packages("tidytuesdayR")
+library(tidyverse, tidytuesdayR, dplyr)
+
+# Set working directory
+setwd("/Users/admin/Documents/7. UCL E:S:PA/R Study Group")
